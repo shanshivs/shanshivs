d6f9e2c1d7ae10bd8400aba7fae105105f348f2c
shanshivs <100437819+shanshivs@users.noreply.github.com>
2022-02-25 16:21:39 -0500

    Create groovy10.groovy

---
+++ groovy10.groovy
@@ -0,0 +1,47 @@
+import groovy.json.JsonSlurper
+import groovy.json.*
+import groovy.json.JsonSlurper
+import groovy.json.JsonBuilder
+
+def jsonSlurper2 = new JsonSlurper()
+
+//update the filepath with the path of your job.
+
+//def jobs = new BufferedReader(new InputStreamReader(new FileInputStream( "/var/lib/jenkins/workspace/testSd/seed2.json"),"UTF-8"))
+//data2 = jsonSlurper2.parse(jobs)
+//def allSJObs = data2.jobs as Object
+//data2.jobs.each
+for (i=0;i=10;i++)
+  {
+    println("Create Job i")
+
+// here starts create the job
+    job("$i") {
+        description(' This is the Description of the job.')
+  
+        logRotator(5, 100, -1, -1)
+        parameters {
+            stringParam('Name', 'DefaultValue', 'This is a first parameter')
+            stringParam('Age', '30', 'This is a second parameter')
+          stringParam('test', '1233430', 'This is a second parameter')
+        }
+        label('master')
+    steps {
+        downstreamParameterized {
+            trigger('paramjob') {
+                block {
+                    buildStepFailure('FAILURE')
+                    failure('FAILURE')
+                    unstable('UNSTABLE')
+                }
+                parameters {
+                    predefinedProp('Name', '$Name')
+                    predefinedProp('Age', '$Age')
+               }
+            }
+        }
+      }
+    }
+
+    ////////////here ends create the job
+  }
